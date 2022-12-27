7493a8a8059977f9d6a3c7d57f1fd7b0b97e84f6
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-12-27 21:35:13 +0900

    update

--- BaekJoon/src/q28_최단경로/a02_1504/Main.java
+++ BaekJoon/src/q28_최단경로/a02_1504/Main.java
@@ -12,66 +12,78 @@ package q28_최단경로.a02_1504;
 첫째 줄에 두 개의 정점을 지나는 최단 경로의 길이를 출력한다. 그러한 경로가 없을 때에는 -1을 출력한다.
 */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
+class Node implements Comparable<Node> {
+    int next, w;
+    Node(int next, int w) {
+        this.next = next;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Node n) {
+        return w - n.w;
+    }
+}
 public class Main {
-	static int V, E;
-	static int visit[];
-	static ArrayList<ArrayList<Integer>> A;
-	static StringBuilder sb = new StringBuilder();
+    static int N, E;
+    static ArrayList<ArrayList<Node>> A;
+    static int[] dp;
+    static boolean[] visit;
+    static final int INF = 200000000;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+ 
+        A = new ArrayList<>();
+        dp = new int[N+1];
+        visit = new boolean[N+1];
 
-		while(N-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			visit = new int[V+1];
-			
-			A = new ArrayList<>();
-			for(int i = 0; i <= V; i++) A.add(new ArrayList<Integer>());
+        for (int i = 0; i <= N; i++)  A.add(new ArrayList<>());
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            A.get(x).add(new Node(y, w));
+            A.get(y).add(new Node(x, w));
+        }
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int res1, res2;
+        dijkstra(1); res1 = dp[a]; res2 = dp[b];
+        dijkstra(a); res1 += dp[b]; res2 += dp[b];
+        dijkstra(N); res1 += dp[b]; res2 += dp[a];
 
-			for(int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				A.get(x).add(y); A.get(y).add(x);
-			}
-			binChk();
-		}
-		System.out.print(sb);
-	}
-
-	public static void binChk() {
-		Queue<Integer> q = new LinkedList<Integer>();
-		for(int i = 1; i <= V; i++) {
-			if(visit[i] == 0) { q.add(i); visit[i] = 1; }
-
-			while(!q.isEmpty()) {
-				int cur = q.poll();
-				int len = A.get(cur).size();
-
-				for(int j = 0; j < len; j++) {
-					int next = A.get(cur).get(j);
-					
-					if (visit[next] == visit[cur]) { sb.append("NO\n"); return; }
-					if (visit[next] == 0) {
-						q.add(next);
-						if(visit[cur] == 1) visit[next] = 2;
-						else visit[next] = 1;
-					}
-				}
-			}
-		}
-		sb.append("YES\n");
-	}
+        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
+        System.out.print(ans);
+    }
+    public static void dijkstra(int x) {
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        boolean[] check = new boolean[N + 1];
+        Arrays.fill(visit, false);
+        Arrays.fill(dp, INF);
+        q.offer(new Node(x, 0));
+        dp[x] = 0;
+ 
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int cur = node.next;
+            
+            if (check[cur]) continue;
+            check[cur] = true;
+            for (Node n : A.get(cur)) {
+                if (!check[n.next] && dp[n.next] > dp[cur] + n.w) {
+                    dp[n.next] = dp[cur] + n.w;
+                    q.add(new Node(n.next, dp[n.next]));
+                }
+            }
+        }
+    }
 }
