3f918f3e0f1bb5ceb3daf1f72f399f7da4de188f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-01-01 20:18:30 +0900

    update

--- BaekJoon/src/q29_투포인터/a05_1450/Main.java
+++ BaekJoon/src/q29_투포인터/a05_1450/Main.java
@@ -1,6 +1,15 @@
 package q29_투포인터.a05_1450;
 /*
+문제
+세준이는 N개의 물건을 가지고 있고, 최대 C만큼의 무게를 넣을 수 있는 가방을 하나 가지고 있다.
 
+N개의 물건을 가방에 넣는 방법의 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N과 C가 주어진다. N은 30보다 작거나 같은 자연수, C는 109보다 작거나 같은 음이 아닌 정수이다. 둘째 줄에 물건의 무게가 주어진다. 무게도 109보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 가방에 넣는 방법의 수를 출력한다.
 */
 
 import java.io.BufferedReader;
@@ -12,7 +21,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer sb = new StringBuffer();
-		StringTokenizer st;
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int[] A = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
 	}
 }
