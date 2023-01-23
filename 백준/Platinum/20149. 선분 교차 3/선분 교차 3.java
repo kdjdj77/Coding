47bc67f42850f566b66c6b97c659402518cd686f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-01-24 02:57:34 +0900

    [Platinum IV] Title: 선분 교차 3, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Platinum/20149. 선분 교차 3/선분 교차 3.java
+++ 백준/Platinum/20149. 선분 교차 3/선분 교차 3.java
@@ -25,7 +25,7 @@ public class Main {
 		int a1 = ccw(B1, B2, A1), a2 = ccw(B1, B2, A2);
 		
 		if (b1 == 0 && b2 == 0 & a1 == 0 & a2 == 0) {
-			int n = isCrossed(A1, A2, B1, B2);
+			int n = overlap(A1, A2, B1, B2);
 			sb.append(n > 0 ? 1 : 0);
 			if (n == 1) {
 				sb.append('\n');
@@ -34,53 +34,46 @@ public class Main {
                 if (A2.x == B1.x && A2.y == B1.y || A2.x == B2.x && A2.y == B2.y)
                 	sb.append(A2.x).append(" ").append(A2.y);    
 			}
-		} else {
-			boolean c1 = b1 * b2 <= 0;
-			boolean c2 = a1 * a2 <= 0;
-			if (c1 && c2) {
-				sb.append(1).append("\n");
-				double[] res = meet(A1, A2, B1, B2);
-				sb.append(res[0]).append(" ").append(res[1]);
-			} else sb.append(0);
-		}
+		} else if (b1 * b2 <= 0 && a1 * a2 <= 0) {
+			sb.append(1).append("\n");
+			double[] res = meet(A1, A2, B1, B2);
+			sb.append(res[0]).append(" ").append(res[1]);
+		} else sb.append(0);
 		System.out.print(sb);
 	}
 	static int ccw(Dot a, Dot b, Dot c) {
-		long res = 0;
-		res += a.x*b.y + b.x*c.y + c.x*a.y;
-		res -= a.y*b.x + b.y*c.x + c.y*a.x;
-		if (res != 0) return res > 0 ? 1 : -1;
+		long l = a.x*b.y + b.x*c.y + c.x*a.y - a.y*b.x - b.y*c.x - c.y*a.x;
+		if (l != 0) return l > 0 ? 1 : -1;
 		return 0;
 	}
-	static double[] meet(Dot a1, Dot a2, Dot b1, Dot b2) {
+	static double[] meet(Dot A1, Dot A2, Dot B1, Dot B2) {
 		double X, Y;
-		if (a2.x-a1.x == 0) {
-			double dYdX = (b2.y-b1.y)/(double)(b2.x-b1.x);
-			X = a1.x;
-			Y = dYdX * (X - b1.x) + b1.y;
-		} else if (b2.x-b1.x == 0) {
-			double dydx = (a2.y-a1.y)/(double)(a2.x-a1.x);
-			X = b1.x;
-			Y = dydx * (X - a1.x) + a1.y;
+		if (A2.x-A1.x == 0) {
+			X = A1.x;
+			Y = (B2.y-B1.y)/(double)(B2.x-B1.x) * (X - B1.x) + B1.y;
+		} else if (B2.x-B1.x == 0) {
+			X = B1.x;
+			Y = (A2.y-A1.y)/(double)(A2.x-A1.x) * (X - A1.x) + A1.y;
 		} else {
-			double dydx = (a2.y-a1.y)/(double)(a2.x-a1.x);
-			double dYdX = (b2.y-b1.y)/(double)(b2.x-b1.x);
-			X = (a1.y - b1.y + dYdX * b1.x - dydx * a1.x) / (dYdX - dydx);
-			Y = dYdX * (X - b1.x) + b1.y;
+			double iA = (A2.y-A1.y)/(double)(A2.x-A1.x);
+			double iB = (B2.y-B1.y)/(double)(B2.x-B1.x);
+			X = (A1.y - B1.y + iB * B1.x - iA * A1.x) / (iB - iA);
+			Y = iB * (X - B1.x) + B1.y;
 		}
 		return new double[] {X, Y};
 	}
-	static int isCrossed(Dot A1, Dot A2, Dot B1, Dot B2) {
-        long minA, maxA, minB, maxB;
+	static int overlap(Dot A1, Dot A2, Dot B1, Dot B2) {
+        long[] sA, sB;
         if (A1.x == A2.x) {
-            minA = Math.min(A1.y, A2.y); maxA = Math.max(A1.y, A2.y);
-            minB = Math.min(B1.y, B2.y); maxB = Math.max(B1.y, B2.y);
+        	sA = new long[] {A1.y, A2.y};
+        	sB = new long[] {B1.y, B2.y};
         } else {
-            minA = Math.min(A1.x, A2.x); maxA = Math.max(A1.x, A2.x);
-            minB = Math.min(B1.x, B2.x); maxB = Math.max(B1.x, B2.x);
+        	sA = new long[] {A1.x, A2.x};
+        	sB = new long[] {B1.x, B2.x};
         }
-        if (minA == maxB || maxA == minB) return 1;
-        else if (minA < maxB && minB < maxA) return 2;
+        Arrays.sort(sA); Arrays.sort(sB);
+        if (sA[0]==sB[1] || sA[1]==sB[0]) return 1;
+        else if (sA[0]<sB[1] && sB[0]<sA[1]) return 2;
         else return 0;
     }
 }
