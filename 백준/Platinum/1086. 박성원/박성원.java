12ba485d3c11c621448ad631970b3c51715136ce
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-01-26 16:49:47 +0900

    [Platinum V] Title: 박성원, Time: 324 ms, Memory: 42596 KB -BaekjoonHub

--- 백준/Platinum/1086. 박성원/박성원.java
+++ 백준/Platinum/1086. 박성원/박성원.java
@@ -1,62 +1,64 @@
 import java.io.*;
-import java.math.*;
-import java.util.*;
  
 public class Main {
-	static BigInteger[] arr;
-	static int N, K, maxBit, mods[][];
 	static long[][] dp;
+	static int N, K, bit, mods[][];
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine());
-		arr = new BigInteger[N];
-		maxBit = (1 << N) - 1;
-		for(int i = 0; i < N; i++) arr[i] = new BigInteger(br.readLine());
-		
-		K = Integer.parseInt(br.readLine());
-		dp = new long[K][maxBit+1];
-		mods = new int[K][N];
-		
-		for(int i = 0; i < K; i++) {
-			Arrays.fill(mods[i], -1);
-			Arrays.fill(dp[i], -1);
-		}
-		
-		long res = dp(0, 0);
-		if(res == 0) sb.append("0/1");
-		else {
-			long fac = fac(N);
-			long gcd = gcd(fac, res);
-			sb.append(res/gcd).append("/").append(fac/gcd);
-		}
-		System.out.print(sb);
-	}
-	static long dp(int mod, int visit) {
-		if(dp[mod][visit] != -1) return dp[mod][visit];
-		if(visit == maxBit) return mod == 0 ? 1 : 0;
-		
-		long res = 0;
-		for(int i = 0; i < N; i++) {
-			int cur = 1 << i;
-			if((visit & cur) == 0) res += dp(mod(mod, i), visit|cur);
-		}
-		return dp[mod][visit] = res;
-	}
-	static int mod(int mod, int idx) {
-		if(mods[mod][idx] != -1) return mods[mod][idx];
-		BigInteger BI = new BigInteger(String.valueOf(mod) + String.valueOf(arr[idx]));
-		return mods[mod][idx] = BI.remainder(BigInteger.valueOf(K)).intValue();
-	}
-	static long gcd(long n, long m) {
-		if (m == 0) return n;
-		return gcd(m, n % m);
-	}
-	static long fac(int n) {
-		long res = 1;
-		for(int i = 2; i <= N; i++) res *= i;
-		return res;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        N = Integer.parseInt(br.readLine());
+        String[] nums = new String[N];
+        mods = new int[N][2];
+        bit = (1 << N);
+        for(int i = 0; i < N; i++) nums[i] = br.readLine();
+
+        K = Integer.parseInt(br.readLine());
+        dp = new long[bit][K];
+        
+        for(int i = 0; i < N; i++) {
+            mods[i][1] = 1;
+            int len = nums[i].length();
+            for(int j = 0; j < len; j++) mods[i][1] = (mods[i][1] * 10) % K;
+            mods[i][0] = mod(nums[i], K);
+            dp[1<<i][mods[i][0]] = 1;
+        }
+        set(0);
+
+        long res = dp[(1<<N)-1][0];
+        long pac = 1;
+        for(int i = 2; i <= N; i++) pac *= i;
+        
+        if(res == 0) sb.append("0/1");
+        else {
+            long gcd = gcd(pac,res);
+            sb.append(res/gcd).append("/").append(pac/gcd);
+        }
+        System.out.print(sb);
+    }
+    static void set(int visit) {
+    	if (visit == bit) return;
+    	for(int idx = 0; idx < N; idx++) {
+        	int cur = 1 << idx;
+            if ((visit & cur) > 0) continue;
+            
+            for(int m = 0; m < K; m++) {
+            	int mod = ((m * mods[idx][1]) % K + mods[idx][0]) % K;
+                dp[visit|cur][mod] += dp[visit][m];
+            }
+        }
+    	set(visit+1);
+    }
+    static int mod(String n, int k) {
+        int res = 0;
+        for(int i = 0; i < n.length(); i++)
+        	res = (res*10 + n.charAt(i) - 48) % k;
+        return res;
+    }
+    static long gcd(long a, long b) {
+        long c = a % b;
+        if(c == 0) return b;
+        return gcd(b,c);
+    }
 }
