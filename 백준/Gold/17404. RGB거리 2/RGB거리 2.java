5eea021d515db8af6fe0f3fad8b183b4b33d44ab
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-01-27 14:19:08 +0900

    [Gold IV] Title: RGB거리 2, Time: 152 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final int INF = 12345678;
+    static int house[][], dp[][], N;
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        N = Integer.parseInt(br.readLine());
+        house = new int[N][3];
+        dp = new int[N][3];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<3; j++) house[i][j] = Integer.parseInt(st.nextToken()); 
+        }
+        int res = INF;
+        for(int i = 0; i < 3; i++) res = Math.min(res, res(i));
+        System.out.println(res);        
+    }
+    static int res(int s) {
+    	int res = INF;
+    	
+    	for(int i = 0; i < 3; i++) dp[0][i] = INF;
+    	dp[0][s] = house[0][s];
+    	
+        for(int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + house[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + house[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + house[i][2];
+        }
+        for(int i = 0; i < 3; i++) if(i != s) res = Math.min(res, dp[N-1][i]);
+        return res;
+    }
+}
