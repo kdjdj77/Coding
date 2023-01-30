7bd413328fa55916ec391e52f176f37ce294f7ac
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-01-30 21:56:51 +0900

    update

--- BaekJoon/src/q99_xxx/a99_xxx/Main.java
+++ BaekJoon/src/q99_xxx/a99_xxx/Main.java
@@ -11,6 +11,65 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer sb = new StringBuffer();
 		StringTokenizer st;
-
+		
+		System.out.println(solution("...!@BaT#*..y.abcdefghijklm"));
+		
+		
 	}
+	public static String solution(String new_id) {
+    	String res = new_id;
+    	res = lv1(res);
+    	System.out.println("- " + res);
+    	res = lv2(res);
+    	System.out.println("- " + res);
+    	res = lv3(res);
+    	System.out.println("- " + res);
+    	res = lv4(res);
+    	System.out.println("- " + res);
+    	res = lv5(res);
+    	System.out.println("- " + res);
+    	res = lv6(res);
+    	System.out.println("- " + res);
+    	res = lv7(res);
+    	System.out.println("- " + res);
+    	return res;
+    }
+	public static String lv1(String s) {
+    	return s.toLowerCase();
+    }
+	public static String lv2(String s) {
+    	String tmp = "";
+    	for(int i = 0; i < s.length(); i++) {
+    		char c = s.charAt(i);
+    		if (c >= 'a' && c <= 'z') tmp += c;
+    		if (c >= '0' && c <= '9') tmp += c;
+    		if (c == '-' || c == '_' || c == '.') tmp += c;
+    	}
+    	return tmp;
+    }
+	public static String lv3(String s) {
+		s = s.replaceAll("..", ".");
+    	while(s.contains("..")) {
+    		s = s.replace("..", ".");
+    	}
+    	return s;
+    }
+	public static String lv4(String s) {
+    	if (s.charAt(0) == '.') s = s.substring(1, s.length());
+    	if (s.charAt(s.length()-1) == '.') s = s.substring(0, s.length()-1);
+    	return s;
+    }
+	public static String lv5(String s) {
+    	if (s.length() == 0) s = "a";
+    	return s;
+    }
+	public static String lv6(String s) {
+    	if (s.length() >= 16) s = s.substring(0, 15);
+    	s = lv4(s);
+    	return s;
+    }
+	public static String lv7(String s) {
+    	while(s.length() < 3) s += s.charAt(s.length()-1);
+    	return s;
+    }
 }
---
+++ Programmers/src/q72410/Solution.java
@@ -0,0 +1,58 @@
+package q72410;
+
+/*
+[문제]
+신규 유저가 입력한 아이디를 나타내는 new_id가 매개변수로 주어질 때, "네오"가 설계한 7단계의 처리 과정을 거친 후의 추천 아이디를 return 하도록 solution 함수를 완성해 주세요.
+
+[제한사항]
+new_id는 길이 1 이상 1,000 이하인 문자열입니다.
+new_id는 알파벳 대문자, 알파벳 소문자, 숫자, 특수문자로 구성되어 있습니다.
+new_id에 나타날 수 있는 특수문자는 -_.~!@#$%^&*()=+[{]}:?,<>/ 로 한정됩니다.
+*/
+
+class Solution {
+    public String solution(String new_id) {
+        return lv7(lv6(lv5(lv4(lv3(lv2(lv1(new_id)))))));
+    }
+    String lv1(String s) {
+    	return s.toLowerCase();
+    }
+	String lv2(String s) {
+		char[] arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr)
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.')
+                sb.append(c);
+        return sb.toString();
+	}
+	String lv3(String s) {
+		s.replace("..", ".");
+		while (s.contains("..")) s = s.replace("..", ".");
+		return s;
+	}
+	String lv4(String s) {
+		if (s.length() > 0 && s.charAt(0) == '.')
+        	s = s.substring(1, s.length());
+        if (s.length() > 0 && s.charAt(s.length() - 1) == '.')
+        	s = s.substring(0, s.length() - 1);
+        return s;
+	}
+	String lv5(String s) {
+		if (s.equals("")) s = "a";
+		return s;
+	}
+	String lv6(String s) {
+		if (s.length() < 16) return s;
+    	s = s.substring(0, 15);
+    	s = lv4(s);
+		return s;
+    }
+	String lv7(String s) {
+		StringBuilder sb = new StringBuilder(s);
+        if (sb.length() <= 2) {
+            char last = sb.charAt(sb.length() - 1);
+            while (sb.length() < 3) sb.append(last);
+        }
+        return sb.toString();
+    }
+}
