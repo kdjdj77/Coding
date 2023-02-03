6406a2c06dcd2f7388e85aea2a424b20d821b3a7
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-02-03 22:15:00 +0900

    update

--- BaekJoon/src/q37_문자열알고리즘1/a05_5670/Main.java
+++ BaekJoon/src/q37_문자열알고리즘1/a05_5670/Main.java
@@ -6,15 +6,48 @@ package q37_문자열알고리즘1.a05_5670;
 import java.io.*;
 import java.util.*;
  
+class Node{
+    Map<Character, Node> next = new HashMap<>();
+    boolean isEnd;
+}
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		for(int i = 0; i < N; i++) {
-			String s = br.readLine();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        Node root = new Node();
+        
+        String str = br.readLine();
+        while(!str.equals("") && str != null) {
+            int N = Integer.parseInt(str);
+            String[] list = new String[N];
+            
+            for(int i = 0; i < N; i++) {
+            	Node cur = root;
+                String s = list[i] = br.readLine();
+                for(int j = 0; j < s.length(); j++) {
+                	char c = s.charAt(j);
+	                cur.next.put(c, cur.next.getOrDefault(c, new Node()));
+	                if (j == s.length()-1) cur.isEnd = true;
+	                cur = cur.next.get(c);
+                }
+            }
+            
+            double res = 0;
+            for(int i = 0; i < N; i++) {
+            	Node cur = root;
+            	String s = list[i];
+            	for(int j = 0; j < s.length(); j++) {
+            		char c = s.charAt(j);
+            		if (cur.next.size() > 1 && j != s.length()-1) res++;
+            		else if (cur.isEnd) res++;
+            		cur = cur.next.get(c);
+            	}
+            }
+            System.out.print(String.format("%.2f\n", res/N));
+            sb.append(String.format("%.2f\n", res/N));
+            str = br.readLine();
+        }
+        System.out.print(sb);
+    }
 }
