ab7f2b97b6d886e39e5b8cc21c531e1795fd553c
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-02-10 18:50:39 +0900

    update

--- BaekJoon/src/q39_최소공통조상/a03_11438/Main.java
+++ BaekJoon/src/q39_최소공통조상/a03_11438/Main.java
@@ -21,73 +21,64 @@ class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
 		int N = Integer.parseInt(br.readLine());
-		int maxDepth = 0;
-		int check = 1;
-		while(check <= N) { // 트리의 깊이 계산
-			check <<= 1;
-			maxDepth++;
-		}
+		int maxFloor = 0;
+		for(int i = 1; i < N; i *= 2) maxFloor++;
 		
-		list=new ArrayList<>();
-		for(int i=0;i<N+1;i++) list.add(new ArrayList<>());
-		depth=new int[N+1];
-		parent=new int[N+1][maxDepth+1];
+		list = new ArrayList<>();
+		for(int i = 0; i <= N; i++) list.add(new ArrayList<>());
+		depth = new int[N+1];
+		parent = new int[N+1][maxFloor+1];
 		
-		for(int i=0;i<N-1;i++) {
-			String[] s=br.readLine().split(" ");
-			int a=Integer.parseInt(s[0]);
-			int b=Integer.parseInt(s[1]);
+		for(int i = 1; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			list.get(a).add(b);
 			list.get(b).add(a);
 		}
-		dfs(1, 1); // 트리 생성
-		setParent(N, maxDepth); // 부모 계산
+		setTree(1, 1);
+		setParent(N, maxFloor);
 		
-		int M=Integer.parseInt(br.readLine());
-		for(int i=0;i<M;i++) {
-			String[] s=br.readLine().split(" ");
-			int a=Integer.parseInt(s[0]);
-			int b=Integer.parseInt(s[1]);
-			System.out.println(solve(a, b, maxDepth));
-			
+		int M = Integer.parseInt(br.readLine());
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(res(a, b, maxFloor)).append("\n");
+		}
+		System.out.print(sb);
+	}
+	static void setTree(int node, int floor) {
+		depth[node] = floor;
+		for(int n : list.get(node)) {
+			if (depth[n] != 0) continue;
+			parent[n][0] = node;
+			setTree(n, floor+1);
 		}
 	}
-	
-	
-	public static void dfs(int node, int cnt) {
-		depth[node]=cnt; // node의 깊이 저장
-		for(int n:list.get(node)) { // node와 연결된 곳 중
-			if(depth[n]!=0) continue; // 방문한 곳은 패스
-			parent[n][0]=node; // n의 첫 번째 부모는 node
-			dfs(n, cnt+1); // n으로 이동
-		}
+	static void setParent(int n, int maxFloor) {
+		for(int i = 1; i < maxFloor; i++)
+			for(int j = 1; j <= n; j++)
+				parent[j][i] = parent[parent[j][i-1]][i-1];
 	}
-	
-	public static void setParent(int n, int maxDepth) {
-		for(int i=1;i<maxDepth;i++) { // 2^i번째 부모 저장
-			for(int j=1;j<=n;j++) {
-				parent[j][i]=parent[parent[j][i-1]][i-1];
-			}
+	static int res(int a, int b, int maxFloor) {
+		if (depth[a] > depth[b]) {
+			int tmp = a;
+			a = b;
+			b = tmp;
 		}
-	}
-	
-	public static int solve(int a, int b, int maxDepth) {
-		if(depth[a]>depth[b]) { // a의 깊이가 b보다 더 작도록
-			int temp=a;
-			a=b;
-			b=temp;
-		}
-		for(int i=maxDepth-1;i>=0;i--) { // a, b가 같은 깊이가 되도록 설정
-			if(depth[a]<=depth[parent[b][i]]) b=parent[b][i];
-		}
+		for(int i = maxFloor-1; i >= 0; i--)
+			if (depth[a] <= depth[parent[b][i]]) b = parent[b][i];
 		
-		if(a==b) return a;
-		// 다르다면
-		for(int i=maxDepth-1;i>=0;i--) { // 같이 깊이를 올리며 
-			if(parent[a][i]!=parent[b][i]) {
-				a=parent[a][i];
-				b=parent[b][i];
+		if(a == b) return a;
+		for(int i = maxFloor-1; i >= 0; i--) {
+			if (parent[a][i] != parent[b][i]) {
+				a = parent[a][i];
+				b = parent[b][i];
 			}
 		}
 		return parent[a][0];
