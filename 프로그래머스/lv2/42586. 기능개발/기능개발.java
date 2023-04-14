3fdede937a895615b2459fbaf9de79e7c7fe0e3f
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-04-14 16:42:32 +0900

    [level 2] Title: 기능개발, Time: 2.35 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,26 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+    	ArrayList<Integer> res = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < speeds.length; i++) {
-        	int d = (100 - progresses[i]) / speeds[i];
-        	if ((100 - progresses[i]) % speeds[i] != 0) d++;
-        	q.add(d);
-        }
+        for(int i = 0; i < speeds.length; i++)
+        	q.add((int) Math.ceil((double)(100 - progresses[i]) / speeds[i]));
         
         int day = q.peek(), cnt = 0;
-        ArrayList<Integer> res = new ArrayList<>();
         while(!q.isEmpty()) {
-        	if (day >= q.peek()) {
-        		q.poll();
-        		cnt++;
-        	} else {
-        		res.add(cnt);
-        		day = q.peek();
-        		cnt = 0;
-        	}
+        	if (day >= q.peek()) { q.poll(); cnt++; continue; }
+    		res.add(cnt);
+    		day = q.peek();
+    		cnt = 0;
         }
-        if (cnt != 0) res.add(cnt);
+        res.add(cnt);
         return res.stream().mapToInt(i->i).toArray();
     }
 }
