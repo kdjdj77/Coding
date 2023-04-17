5ec5d93fc779a3704e3ca574ddf344e7957aa7b7
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-04-17 16:26:12 +0900

    [level 2] Title: 주식가격, Time: 25.09 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,29 +1,20 @@
 import java.util.*;
 
-class P {
-	int t;
-	int p;
-	P (int t, int p) {
-		this.t = t;
-		this.p = p;
-	}
-}
 class Solution {
     public int[] solution(int[] prices) {
     	int[] res = new int[prices.length];
-    	Stack<P> s = new Stack<>();
-    	s.add(new P(-1, 0));
+    	Stack<Integer> s = new Stack<>();
     	
         for(int t = 0; t < prices.length; t++) {
-        	while (s.peek().p > prices[t]) {
-        		res[s.peek().t] = t - s.peek().t;
+        	while (!s.isEmpty() && prices[s.peek()] > prices[t]) {
+        		res[s.peek()] = t - s.peek();
         		s.pop();
         	}
-        	s.add(new P(t, prices[t]));
+        	s.add(t);
         }
-        while(s.size() > 1) {
-        	P tmp = s.pop();
-        	res[tmp.t] = prices.length - tmp.t - 1;
+        while(!s.isEmpty()) {
+        	int tmp = s.pop();
+        	res[tmp] = prices.length - tmp - 1;
         }
         return res;
     }
