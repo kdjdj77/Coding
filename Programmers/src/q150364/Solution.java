6a392f206b7f62d2bbe8e8337ae79e99dbbe3b35
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-05-04 16:35:56 +0900

    Update Solution.java

--- Programmers/src/q150364/Solution.java
+++ Programmers/src/q150364/Solution.java
@@ -9,35 +9,10 @@ import java.util.*;
 class Solution {
 	private int N, T[], res[];
     public int[] solution(int[][] edges, int[] target) {
-    	T = target;
-    	N = edges.length;
-    	boolean[] shut = new boolean[N];
     	
-    	Arrays.sort(edges, new Comparator<>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0];
-			}
-    	});
     	
-    	Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-    	for(int i = 0; i <= N; i++) map.put(i+1, new ArrayList<>());
-    	for(int i = 0; i < N; i++) map.get(edges[i][0]).add(i);
-    	
-    	int[] tmp = new int[N+1];
-    	calc(tmp);
     	
         return new int[] {};
-    }
-    private void calc(int[] tmp) {
-    	while(true) {
-    		
-    	}
-    }
-    private int[] move(int num, int[] tmp) {
-    	while(true) {
-    		
-    	}
     }
     // -1:불가, 0:일치, 1:가능
     private int isOk(int[] tmp) {
