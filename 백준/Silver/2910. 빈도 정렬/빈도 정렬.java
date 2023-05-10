09855eb5c576e75be28d5b2aaccf478a48c96ba9
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-05-10 15:42:18 +0900

    [Silver III] Title: 빈도 정렬, Time: 140 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -4,33 +4,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(st.nextToken());
-		int o = 0;
-		
-		Map<Integer, Integer> order = new HashMap<>();
-		Map<Integer, Integer> map = new HashMap<>();
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if (map.containsKey(num)) map.put(num, map.get(num) + 1);
-			else {
-				map.put(num, 1);
-				order.put(num, o++);
-			}
-		}
-		ArrayList<Integer> arr = new ArrayList<>();
-		for(int a : map.keySet()) arr.add(a);
-		Collections.sort(arr, new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				if (map.get(o1) == map.get(o2)) return order.get(o1) - order.get(o2);
-				else return map.get(o2) - map.get(o1);
-			}
-		});
-		for(int a : arr) for(int i = 0; i < map.get(a); i++) sb.append(a).append(" ");
-		System.out.print(sb);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        ArrayList<Integer> arr = new ArrayList<>(map.keySet());
+        arr.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return map.get(o2) - map.get(o1);
+            }
+        });
+        
+        for(int n : arr) for(int i = 0; i < map.get(n); i++) sb.append(n).append(" ");
+        System.out.print(sb);
     }
 }
