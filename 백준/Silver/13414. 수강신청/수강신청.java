7968d9012710ed5590adf23e1f03ab313fb26e6c
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-05-15 14:55:00 +0900

    [Silver III] Title: 수강신청, Time: 760 ms, Memory: 65580 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new LinkedHashSet<>();
+		while(N-- > 0) {
+			String num = br.readLine();
+			if (set.contains(num)) set.remove(num);
+			set.add(num);
+		}
+		for(String n : set) {
+			sb.append(n).append("\n");
+			if (--K == 0) break;
+		}
+		System.out.print(sb);
+    }
+}
