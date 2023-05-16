3106cc868a71bf6949ff2d1326611a758d1a1d9e
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-05-16 21:44:28 +0900

    [Gold IV] Title: 세 수의 합, Time: 372 ms, Memory: 56312 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -6,25 +6,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()), U[] = new int[N];
 		for(int i = 0; i < N; i++) U[i] = Integer.parseInt(br.readLine());
+		br.close();
 		
-		ArrayList<Integer> sum = new ArrayList<>();
-		for(int x = 0; x < N; x++) for(int y = 0; y < N; y++) sum.add(U[x] + U[y]);
 		Arrays.sort(U);
-		Collections.sort(sum);
+		Set<Integer> xySum = new HashSet<>();
+		for(int x : U) for(int y : U) xySum.add(x+y);
 		
-		int res = 0;
-		for(int k = N-1; k >= 0; k--) for(int z = 0; z < k; z++)
-			if (bs(U[k]-U[z], sum)) res = Math.max(res, U[k]);
-		System.out.print(res);
-	}
-	private static boolean bs(int num, ArrayList<Integer> sum) {
-		int s = 0, e = sum.size()-1;
-		while(s < e) {
-			int m = (s + e) / 2;
-			if (sum.get(m) > num) e = m-1;
-			else if (sum.get(m) < num) s = m+1;
-			else return true;
-		}
-		return false;
+		for (int k = N-1; k >= 0; k--) for (int z = N-1; z >= 0; z--)
+			if (xySum.contains(U[k]-U[z])) { System.out.print(U[k]); return; }
 	}
 }
