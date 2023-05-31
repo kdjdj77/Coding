caf09abdb0833adc64fc683422039c5b39c19221
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-05-31 18:59:26 +0900

    [Gold II] Title: 보석 도둑, Time: 1844 ms, Memory: 198260 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -6,21 +6,19 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
- 
-        int[] gems[] = new int[N][2], bags = new int[K];
-        for (int i = 0; i < N; i++)
-        	gems[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (int i = 0; i < K; i++) bags[i] = Integer.parseInt(br.readLine());
-        Arrays.sort(gems, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
-        Arrays.sort(bags);
-        
-        long res = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0, idx = 0; i < K; i++) {
-            while (idx < N && gems[idx][0] <= bags[i]) pq.add(gems[idx++][1]);
-            if (!pq.isEmpty()) res += pq.poll();
-        }
-        System.out.print(res);
+  		PriorityQueue<int[]> gems = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
+  		PriorityQueue<Integer> bags = new PriorityQueue<>();
+  		while(N-- > 0) gems.add(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());
+  		for(int i = 0; i < K; i++) bags.add(Integer.parseInt(br.readLine()));
+  		
+  		long res = 0;
+  		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+  		while(K-- > 0) {
+  			int bag = bags.poll();
+  			while(!gems.isEmpty() && gems.peek()[0] <= bag) pq.add(gems.poll()[1]);
+  			if(!pq.isEmpty()) res += pq.poll();
+  		}
+  		System.out.println(res);
     }
 }
