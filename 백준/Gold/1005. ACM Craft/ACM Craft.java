8e7b49248f7d34e78800e27c0338a81fdbbeb34e
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-07 22:35:08 +0900

    [Gold III] Title: ACM Craft, Time: 800 ms, Memory: 242560 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -2,47 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int[] cost, depth;
-	private static List<Set<Integer>> link;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
- 
+        
         int T = Integer.parseInt(br.readLine());
-        while(T-- > 0) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
-            link = new ArrayList<>();
-            cost = new int[N+1];
-            depth = new int[N+1];
-            
+
+            ArrayList<Integer>[] link = new ArrayList[N + 1];
+            int[] cost = new int[++N], need = new int[N];
             st = new StringTokenizer(br.readLine());
-            for(int i = 0; i <= N; i++) link.add(new HashSet<>());
-            for(int i = 1; i <= N; i++) cost[i] = Integer.parseInt(st.nextToken());
+            for(int i = 1; i < N; i++) cost[i] = Integer.parseInt(st.nextToken());
+            for(int i = 1; i < N; i++) link[i] = new ArrayList<>();
             while(K-- > 0) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-                link.get(a).add(b);
-                depth[b]++;
+                link[a].add(b);
+                need[b]++;
             }
-            sb.append(calc(N, Integer.parseInt(br.readLine()))).append("\n");    
-        }
-        System.out.println(sb);
-    }
-    private static int calc(int N, int W) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] dp = new int[N+1];
-        for(int i = 1; i <= N; i++) {
-            dp[i] = cost[i];
-            if (depth[i] == 0) q.add(i);
-        }
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            for(int n : link.get(cur)) {
-                dp[n] = Math.max(dp[n], dp[cur] + cost[n]);
-                if(--depth[n] == 0) q.add(n);
+            int W = Integer.parseInt(br.readLine()), dp[] = new int[N];
+            Queue<Integer> q = new LinkedList<>();
+            for(int i = 1; i < N; i++) if (need[i] == 0) q.add(i);
+            while(!q.isEmpty()) {
+                int cur = q.poll();
+                dp[cur] += cost[cur];
+                for (int n : link[cur]) {
+                    dp[n] = Math.max(dp[n], dp[cur]);
+                    if (--need[n] == 0) q.add(n);
+                }
             }
+            sb.append(dp[W]).append("\n");
         }
-        return dp[W];
+        System.out.print(sb);
     }
 }
