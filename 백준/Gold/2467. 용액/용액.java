206c8d98e46fbe56208bf83de2d0453e48559c4d
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-13 18:31:54 +0900

    [Gold V] Title: 용액, Time: 368 ms, Memory: 30056 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -4,17 +4,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()), l = 0, r = N-1, a = 0, b = 0, c = (1<<31)-1;
         int[] sol = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int l = 0, r = N-1, sum = Integer.MAX_VALUE, res[] = {0, 0};
         while(l < r) {
-        	int ls = sol[l], rs = sol[r], mix = ls + rs;
-        	if (Math.abs(mix) < Math.abs(sum)) {
-        		res = new int[] {ls, rs};
-        		if (0 == (sum = mix)) break;
+        	int sum = sol[l] + sol[r];
+        	if (Math.abs(sum) < Math.abs(c)) {
+        		a = sol[l]; b = sol[r];
+        		if ((c = sum) == 0) break;
         	}
-        	N = mix < 0 ? l++ : r--;
+        	N = sum < 0 ? l++ : r--;
         }
-        System.out.print(new StringBuilder().append(res[0]).append(" ").append(res[1]));
+        System.out.print(new StringBuilder().append(a).append(" ").append(b));
     }
 }
