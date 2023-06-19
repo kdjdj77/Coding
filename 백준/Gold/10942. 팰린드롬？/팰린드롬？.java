5e0d326eb72e02bf247b08210170671eefffaa95
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-19 19:34:07 +0900

    [Gold IV] Title: 팰린드롬?, Time: 860 ms, Memory: 222516 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -8,26 +8,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
-        N = Integer.parseInt(br.readLine())+1;
-        arr = new int[N];
+        N = Integer.parseInt(br.readLine()); arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
         
-        dp = new boolean[N][N];
-        visit = new boolean[N][N];
-        for(int s = 1; s < N; s++) for(int e = s; e < N; e++) set(s, e);
+        dp = new boolean[N][N]; visit = new boolean[N][N];
+        for(int s = 0; s < N; s++) for(int e = s; e < N; e++) setDP(s, e);
         int M = Integer.parseInt(br.readLine());
         while(M-- > 0) {
         	st = new StringTokenizer(br.readLine());
         	int s = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
-        	sb.append(dp[s][e] ? 1 : 0).append("\n");
+        	sb.append(dp[s-1][e-1] ? 1 : 0).append("\n");
         }
         System.out.print(sb);
     }
-    static void set(int s, int e) {
+    static void setDP(int s, int e) {
 		if (visit[s][e]) return;
 		if (isPal(s, e)) while(s <= e) {visit[s][e] = true; dp[s++][e--] = true;}
-		else dp[s][e] = false;
+		else {visit[s][e] = true; dp[s][e] = false;}
     }
     static boolean isPal(int s, int e) {
     	while(s <= e) if (arr[s++] != arr[e--]) return false;
