361b210d91234496f3d2c34eb015e61b0e2a115d
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-20 18:37:31 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1484 ms, Memory: 332608 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -6,28 +6,18 @@ public class Main {
 	static StringTokenizer st;
 	static int road[];
 	public static void main(String[] args) throws IOException {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
 		st = new StringTokenizer(br.readLine());
-		int N = in(), M = in(), tmp = 0, res = 0;
-		road = new int[N+1];
-		for(int i = 1; i <= N; i++) road[i] = i;
-		
-		PriorityQueue<int[]> path = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
-		while(M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			path.add(new int[] {in(), in(), in()});
-		}
-		while(!path.isEmpty()) {
-			int[] n = path.poll();
-			if (!isSameRoot(n[0], n[1])) {union(n[0], n[1]); res += tmp = n[2];}
+		int N = in(), M = in(), tmp = 0, res = 0; road = new int[N+1];
+		while(tmp++ < N) road[tmp] = tmp;
+		while(M-- > 0) {st = new StringTokenizer(br.readLine()); pq.add(new int[] {in(), in(), in()});}
+		while(!pq.isEmpty()) {
+			int n[] = pq.poll(), r1 = root(n[0]), r2 = root(n[1]);
+			if (r1 != r2) {union(r1, r2); res += tmp = n[2];}
 		}
 		System.out.print(res - tmp);
 	}
 	static int in() {return Integer.parseInt(st.nextToken());}
-	static boolean isSameRoot(int x, int y) {return root(x) == root(y);}
-	static int root(int x) { return x == road[x] ? x : (road[x] = root(road[x])); }
-    static void union(int x, int y) {
-        x = root(x); y = root(y);
-        if (x < y) road[y] = x;
-        else road[x] = y;
-    }
+	static int root(int x) {return road[x] != x ? road[x] = root(road[x]) : x;}
+    static void union(int x, int y) {if (x < y) road[y] = x; else road[x] = y;}
 }
