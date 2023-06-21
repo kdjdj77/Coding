0ad64ebfd78e6ce43cbf197039150be66c15feb6
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-21 17:43:03 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 372 ms, Memory: 23916 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -11,13 +11,13 @@ public class Main {
 		map = new char[N][M];
 		dir = Map.of('U', -10,'D', 10,'L', -1,'R', 1);
 		for(int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
-		for(int d = 0, k = 1; d < N*M; d++) res += dfs(d/M, d%M, k++);
+		for(int d = 0; d < N*M; d++) res += dfs(d/M, d%M);
 		System.out.print(res);
 	}
-	static int dfs(int x, int y, int k) {
+	static int dfs(int x, int y) {
 		if ((c = map[x][y]) < 2) return map[x][y] == 1 ? 1 : 0;
 		map[x][y] = 1;
-		int d = dir.get(c), res = dfs(x+d/10, y+d%10, k);
+		int d = dir.get(c), res = dfs(x+d/10, y+d%10);
 		map[x][y] = 0;
 		return res;
 	}
