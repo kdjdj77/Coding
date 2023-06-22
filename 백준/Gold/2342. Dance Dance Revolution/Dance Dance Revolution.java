d2a8ae37657138d9a90c0c546f173ab287901fd3
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-22 22:58:31 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 268 ms, Memory: 25572 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -4,19 +4,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] cmds = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[][] move = {{1,2,2,2,2},{2,1,3,4,3},{2,3,1,3,4},{2,4,3,1,3},{2,3,4,3,1}};
-        int cur = 0, INF = 4567890, dir[] = new int[5];
+        int cur = 0, dir[] = new int[5], cmd, INF = 4567890;
         for(int i = 1; i < 5; i++) dir[i] = INF;
-        for(int cmd : cmds) {
-        	if (cmd == 0) break;
+        while((cmd = Integer.parseInt(st.nextToken())) != 0) {
             int min = INF, cost = move[cur][cmd];
-            for(int i = 0; i < 5; i++) {
-                min = Math.min(min, dir[i]+move[i][cmd]);
-                dir[i] += cost;
-            }
-            dir[cur] = min;
-            cur = cmd;
+            for(int i = 0; i < 5; dir[i++]+=cost) min = Math.min(min, dir[i] + move[i][cmd]);
+            dir[cur] = min; cur = cmd;
         }
         System.out.print(Arrays.stream(dir).min().orElse(0));
     }
