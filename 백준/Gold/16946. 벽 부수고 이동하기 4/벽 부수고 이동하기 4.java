2b4b1b7ee366a97e8d2abfba0ff22609ce1d5db5
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-27 20:28:16 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 828 ms, Memory: 160452 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -8,35 +8,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
 		map = new int[N = Integer.parseInt(st.nextToken())][M = Integer.parseInt(st.nextToken())];
 		for(int i = 0; i < N; i++, br.readLine()) for(int j = 0; j < M; j++) map[i][j] = br.read()-49;
-		
 		set = new HashMap<>();
-		for(int i = 0, num = 1; i < N; i++) for(int j = 0; j < M; j++)
-			if (map[i][j] == -1) set.put(num, dfs(i, j, num++));
-		
+		for(int i = 0, num = 1; i < N*M; i++) if (map[i/M][i%M] == -1) set.put(num, dfs(i/M, i%M, num++));
 		for(int i = 0; i < N; i++, sb.append("\n")) for(int j = 0; j < M; j++) sb.append(res(i, j));
 		System.out.print(sb);
 	}
 	static boolean isValid(int x, int y) {return x >= 0 && y >= 0 && x < N && y < M;}
 	static int dfs(int x, int y, int num) {
-		int res = 1;
+		int x1, y1, res = 1;
 		map[x][y] = num;
-		for(int[] d : dir) {
-			int x1 = x+d[0], y1 = y+d[1];
-			if (isValid(x1, y1) && map[x1][y1] == -1) res += dfs(x1, y1, num);
-		}
+		for(int[] d : dir) if (isValid(x1=x+d[0], y1=y+d[1]) && map[x1][y1] == -1) res += dfs(x1, y1, num);
 		return res;
 	}
 	static int res(int x, int y) {
 		if (map[x][y] != 0) return 0;
-		int res = 1;
+		int x1, y1, res = 1;
 		Set<Integer> nearby = new HashSet<>();
-		for(int d[] : dir) {
-			int x1 = x+d[0], y1 = y+d[1];
-			if (isValid(x1, y1) && map[x1][y1] != 0) nearby.add(map[x1][y1]);
-		}
+		for(int d[] : dir) if (isValid(x1=x+d[0], y1=y+d[1]) && map[x1][y1] != 0) nearby.add(map[x1][y1]);
 		for(int num : nearby) res += set.get(num);
 		return res % 10;
 	}
