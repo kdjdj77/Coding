228664d51575044368eff6b1a6f65bf8db0942de
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-29 23:12:58 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 196 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -2,20 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static char[] str;
+	static char[] S;
 	static int len, dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Arrays.fill(dp = new int[len = (str = br.readLine().toCharArray()).length], len);
-        dp[dp[0] = 1] = str[0] == str[1] ? 1 : 2;
-        for(int i = 2; i < len; i++) {
-            dp[i] = Math.min(dp[i-1]+1, dp[i]);
-            for(int j = 0; j < 2; j++) if (str[i] == str[i-1-j]) pal(i-j, i-1);
+        Arrays.fill(dp = new int[len = (S = br.readLine().toCharArray()).length + 1], len);
+        dp[2] = S[dp[0] = 0] == S[dp[1] = 1] ? 1 : 2;
+        for(int i = 2; i < len-1; i++) {
+            dp[i+1] = Math.min(dp[i]+1, dp[i+1]);
+            for(int j = 0; j < 2; j++) if (S[i] == S[i-1-j]) pal(i-j, i-1);
         }
         System.out.print(dp[len-1]);
     }
     static void pal(int s, int e) {
-        while(0 < s && e < len-1 && str[--s] == str[++e])
-            dp[e] = s == 0 ? 1 : Math.min(dp[e], dp[s-1]+1);
+        while(0 < s && e < len-2 && S[--s] == S[++e]) dp[e+1] = Math.min(dp[e+1], dp[s]+1);
     }
 }
