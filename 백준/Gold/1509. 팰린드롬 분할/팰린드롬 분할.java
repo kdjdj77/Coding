0159737d2bc7944c85dec3c91f110ca67594c1d4
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-06-29 23:33:04 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 184 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -6,15 +6,12 @@ public class Main {
 	static int len, dp[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Arrays.fill(dp = new int[len = (S = br.readLine().toCharArray()).length + 1], len);
+        Arrays.fill(dp = new int[len = (S = br.readLine().toCharArray()).length + 1], len--);
         dp[2] = S[dp[0] = 0] == S[dp[1] = 1] ? 1 : 2;
-        for(int i = 2; i < len-1; i++) {
-            dp[i+1] = Math.min(dp[i]+1, dp[i+1]);
-            for(int j = 0; j < 2; j++) if (S[i] == S[i-1-j]) pal(i-j, i-1);
-        }
-        System.out.print(dp[len-1]);
+        for(int i = 2; i < len; pal(i, i-1), pal(i-1, i-1), i++) dp[i+1] = Math.min(dp[i+1], dp[i]+1);
+        System.out.print(dp[len]);
     }
     static void pal(int s, int e) {
-        while(0 < s && e < len-2 && S[--s] == S[++e]) dp[e+1] = Math.min(dp[e+1], dp[s]+1);
+        while(0 < s-- && ++e < len && S[s] == S[e]) dp[e+1] = Math.min(dp[e+1], dp[s]+1);
     }
 }
