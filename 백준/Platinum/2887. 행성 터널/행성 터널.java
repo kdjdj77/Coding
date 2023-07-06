9bd214bf1e215d0c1993285b77d544de081a2080
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-07-06 19:47:12 +0900

    [Platinum V] Title: 행성 터널, Time: 1056 ms, Memory: 75964 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -2,25 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+	static PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]), pqs[] = new PriorityQueue[3];
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int p, root[];
-	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine()), S[][] = new int[N][4], res = 0;
-		root = new int[N];
-		for(int i = 0; i < N; i++) S[i] = new int[] {root[i] = st(i), in(), in(), in()};
-		for(p = 1; p < 4; p++) {
-			Arrays.sort(S, (p1, p2) -> p1[p] - p2[p]);
-			for(int i = 1; i < N; i++) pq.add(new int[] {S[i-1][0], S[i][0], Math.abs(S[i-1][p]-S[i][p])});
+	static int N, res = 0, cnt = 0, root[];
+	public static void main(String[] args) throws IOException{
+		root = new int[N = Integer.parseInt(br.readLine())];
+		for(int i = 0; i < 3; i++) pqs[i] = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+		for(int i = 0; i < N; root[i] = i++) for(int j = st(0); j < 3; j++) pqs[j].add(new int[] {i, in()});
+		for(int i = 0; i < 3; i++) for(int j = 0; j < N-1; j++) {
+			int[] cur = pqs[i].poll(), next = pqs[i].peek();
+			pq.add(new int[] {cur[0], next[0], Math.abs(cur[1]-next[1])});
 		}
 		while(!pq.isEmpty()) {
-			int cur[] = pq.poll(), s = find(cur[0]), e = find(cur[1]);
-			if (s != e) {res += cur[2]; root[e] = s;}
+			int edge[] = pq.poll(), s = root(edge[0]), e = root(edge[1]);
+			if (s != e) {res += edge[2]; root[e] = s; if (++cnt == N-1) break;}
 		}
-		System.out.print(res);
+		System.out.println(res);
 	}
 	static int st(int n) {try {st = new StringTokenizer(br.readLine());} catch (IOException e) {} return n;}
-	static int find(int x) {return root[x] == x ? x : (root[x] = find(root[x]));}
 	static int in() {return Integer.parseInt(st.nextToken());}
+	static int root(int n) {return root[n] == n ? n : (root[n] = root(root[n]));}
 }
