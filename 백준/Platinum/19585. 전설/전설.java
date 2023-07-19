48abe07eaf6cdf145e0e37140c733e9cb9e2d902
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-07-19 20:11:05 +0900

    [Platinum III] Title: 전설, Time: 6036 ms, Memory: 875052 KB -BaekjoonHub

--- 백준/Platinum/19585. 전설/전설.java
+++ 백준/Platinum/19585. 전설/전설.java
@@ -5,29 +5,26 @@ public class Main {
 	static class Trie {
 	    boolean isEnd = false;
 	    Trie[] next = new Trie[26];
-	    Trie add(char c) {return next[c-'a'] = next[c-'a'] == null ? new Trie() : next[c-'a'];}
+	    Trie add(int n) {return next[n] = next[n] == null ? new Trie() : next[n];}
 	}
     static Set<String> name = new HashSet<>();
-    static StringTokenizer st;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         Trie root = new Trie();
-        st = new StringTokenizer(read());
-        int C = in(), N = in();
+        int C = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken());
         while(C-- > 0) {
             Trie cur = root;
-            String str = read();
-            for(int i = 0, len = str.length(); i < len; i++) cur = cur.add(str.charAt(i));
+            String str = br.readLine();
+            for(int i = 0, len = str.length(); i < len; i++) cur = cur.add(str.charAt(i)-97);
             cur.isEnd = true;
         }
-        while(N-- > 0) name.add(read());
-        int Q = Integer.parseInt(read());
-        while(Q-- > 0) sb.append(res(read(), root) ? "Yes\n" : "No\n");
+        while(N-- > 0) name.add(br.readLine());
+        int Q = Integer.parseInt(br.readLine());
+        while(Q-- > 0) sb.append(res(br.readLine(), root) ? "Yes\n" : "No\n");
         System.out.print(sb);
     }
-    static String read() throws IOException {return br.readLine();}
-    static int in() {return Integer.parseInt(st.nextToken());}
     static boolean res(String str, Trie root) {
         Trie cur = root;
         for(int i = 0, len = str.length(); i < len; i++) {
