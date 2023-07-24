0488d3eeac65ab2069e52260bfd81e7b051cbe3d
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-07-24 18:16:47 +0900

    [Platinum IV] Title: 단절선, Time: 952 ms, Memory: 155836 KB -BaekjoonHub

--- 백준/Platinum/11400. 단절선/단절선.java
+++ 백준/Platinum/11400. 단절선/단절선.java
@@ -4,22 +4,18 @@ import java.util.*;
 public class Main {
 	static int V, E, N[], cnt = 0;
 	static ArrayList<Integer>[] link;
-	static TreeSet<int[]> res = new TreeSet<>((a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+	static ArrayList<int[]> res = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-    	st(); V = in()+1; E = in();
-        link = new ArrayList[V];
-        N = new int[V+1];
+    	Reader fr = new Reader();
+        link = new ArrayList[(V = fr.ni()+1)]; N = new int[V+1]; E = fr.ni();
         for(int i = 1; i < V; i++) link[i] = new ArrayList<>();
-        for(int i = 0, a, b; i < E; i++) {st(); link[a=in()].add(b=in()); link[b].add(a);}
+        for(int i = 0, a, b; i < E; i++) {link[a=fr.ni()].add(b=fr.ni()); link[b].add(a);}
         for(int i = 1; i < V; i++) if (N[i] == 0) dfs(1, 0);
+        Collections.sort(res, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
         StringBuilder sb = new StringBuilder().append(res.size()).append("\n");
         for(int[] r : res) sb.append(r[0]).append(" ").append(r[1]).append("\n");
         System.out.print(sb);
     }
-    static int in() {return Integer.parseInt(st.nextToken());}
-    static void st() throws IOException {st = new StringTokenizer(br.readLine());}
     static int[] arr(int a, int b) {return a < b ? new int[] {a, b} : new int[] {b, a};}
     static int dfs(int cur, int before) {
     	int min = N[cur] = ++cnt, min2;
@@ -31,4 +27,10 @@ public class Main {
         }
     	return min;
     }
+}
+class Reader {
+	BufferedReader br; StringTokenizer st;
+    public Reader(){br=new BufferedReader(new InputStreamReader(System.in));}
+    String ns(){if(st==null||!st.hasMoreTokens())try{st=new StringTokenizer(br.readLine());}catch(Exception e){}return st.nextToken();}
+    int ni(){return Integer.parseInt(ns());}
 }
