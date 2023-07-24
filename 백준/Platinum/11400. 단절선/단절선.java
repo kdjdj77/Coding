da2b7214c6846497dea33ecfa24cb16f3dd88ae0
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-07-24 19:54:00 +0900

    [Platinum IV] Title: 단절선, Time: 952 ms, Memory: 143456 KB -BaekjoonHub

--- 백준/Platinum/11400. 단절선/단절선.java
+++ 백준/Platinum/11400. 단절선/단절선.java
@@ -2,35 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static class Line implements Comparable<Line> {
+		int a, b; 
+		Line(int x, int y) {a = x; b = y;}
+		@Override
+		public int compareTo(Line o) {return a == o.a ? b - o.b : a - o.a;}
+	}
 	static int V, E, N[], cnt = 0;
 	static ArrayList<Integer>[] link;
-	static ArrayList<int[]> res = new ArrayList<>();
+	static ArrayList<Line> res = new ArrayList<>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
-    	Reader fr = new Reader();
-        link = new ArrayList[(V = fr.ni()+1)]; N = new int[V+1]; E = fr.ni();
+    	st(); link = new ArrayList[(V = in()+1)]; N = new int[V]; E = in();
         for(int i = 1; i < V; i++) link[i] = new ArrayList<>();
-        for(int i = 0, a, b; i < E; i++) {link[a=fr.ni()].add(b=fr.ni()); link[b].add(a);}
-        for(int i = 1; i < V; i++) if (N[i] == 0) dfs(1, 0);
-        Collections.sort(res, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        for(int i = 0, a, b; i < E; i++) {st(); link[a=in()].add(b=in()); link[b].add(a);}
+        br.close(); dfs(1, 0);
+        Collections.sort(res);
         StringBuilder sb = new StringBuilder().append(res.size()).append("\n");
-        for(int[] r : res) sb.append(r[0]).append(" ").append(r[1]).append("\n");
+        for(Line r : res) sb.append(r.a).append(" ").append(r.b).append("\n");
         System.out.print(sb);
     }
-    static int[] arr(int a, int b) {return a < b ? new int[] {a, b} : new int[] {b, a};}
+    static int in() {return Integer.parseInt(st.nextToken());}
+    static void st() throws IOException {st = new StringTokenizer(br.readLine());}
     static int dfs(int cur, int before) {
-    	int min = N[cur] = ++cnt, min2;
+    	int min = N[cur] = ++cnt;
     	for(int next : link[cur]) {
-    		if (next == before) continue;
-    		if (N[next] != 0) min2 = N[next];
-    		else if ((min2 = dfs(next, cur)) > N[cur]) res.add(arr(cur, next));
+    		if (before == next) continue;
+    		if (N[next] != 0) {min = Math.min(min, N[next]); continue;}
+    		int min2 = dfs(next, cur);
+    		if (min2 > N[cur]) res.add(cur < next ? new Line(cur, next) : new Line(next, cur));
             min = Math.min(min, min2);
         }
     	return min;
     }
-}
-class Reader {
-	BufferedReader br; StringTokenizer st;
-    public Reader(){br=new BufferedReader(new InputStreamReader(System.in));}
-    String ns(){if(st==null||!st.hasMoreTokens())try{st=new StringTokenizer(br.readLine());}catch(Exception e){}return st.nextToken();}
-    int ni(){return Integer.parseInt(ns());}
 }
