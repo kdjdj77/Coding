16a1bbe1f7edc52000edd65c5fe50aa78e0fcd4a
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-08-08 19:08:04 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 132 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,16 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-	public static void main(String[] args)throws Exception{
+public class Main {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()), n = 1, S[] = new int[1001], i = -1;
+		int next = 1; br.readLine();
+		Stack<Integer> s = new Stack<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(N-- > 0) {
-			while(i >= 0 && S[i] == n) {i--; n++;}
-			S[++i] = Integer.parseInt(st.nextToken());
+		while(st.hasMoreTokens()) {
+			while(!s.isEmpty() && s.peek() == next) {s.pop(); next++;}
+			s.push(Integer.parseInt(st.nextToken()));
+			while(!s.isEmpty() && s.peek() == next) {s.pop(); next++;}
 		}
-		while(i >= 0 && S[i] == n) {i--; n++;}
-		System.out.print(i == -1 ? "Nice" : "Sad");
+		System.out.print(s.isEmpty() ? "Nice" : "Sad");
 	}
 }
