492ab5aaef664f85ef397d45d8b7bdbc0ed8fc52
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-08-16 22:54:46 +0900

    Update Main.java

--- BaekJoon/src/q21_큐덱/a_24511/Main.java
+++ BaekJoon/src/q21_큐덱/a_24511/Main.java
@@ -73,38 +73,24 @@ $C$의 원소를 차례대로 queuestack에 삽입했을 때의 리턴값을 공
 */
 
 
-import java.io.*;	
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
-                new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =
-                new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        // arr[i][0] = 0이면 큐, 1이면 스택
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(); br.readLine();
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] QS = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        // 스택에 대해선 신경쓸 필요가 없으므로 큐 자료구조인 것만 저장
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (arr[i] == 0)
-                deque.addLast(num);
-        }
+        for(int f : QS) if (f == 0) dq.addLast(Integer.parseInt(st.nextToken()));
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<M; i++) {
-            deque.addFirst(Integer.parseInt(st.nextToken()));
-            bw.write(deque.pollLast()+" ");
+        while(M-- > 0) {
+            dq.addFirst(Integer.parseInt(st.nextToken()));
+            sb.append(dq.pollLast()).append(" ");
         }
-        bw.flush();
+        System.out.print(sb);
     }
 }
