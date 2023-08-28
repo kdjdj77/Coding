17fc04dc2864aeee00d96b56a93838f830f64647
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-08-28 22:43:34 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 136 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,13 +1,11 @@
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine()), p = n;
-		for(long i = 2; i*i <= n; i++) {
-			if (n % i == 0) p = p/i*(i-1);
-			while(n % i == 0) n /= i;
+        long n = Long.parseLong(new BufferedReader(new InputStreamReader(System.in)).readLine()), p=n;
+		for(long i=2; i*i<=n; i++) {
+			if (n%i==0) p-=p/i;
+			while(n%i==0) n/=i;
 		}
-		System.out.print(n == 1 ? p : p/n*(n-1));
+		System.out.print(n==1?p:p-p/n);
     }
 }
