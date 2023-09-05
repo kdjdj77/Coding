b23d706e6839a8be3c44caadd7695efefb3b16f7
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-09-05 22:00:34 +0900

    Update Main.java

--- BaekJoon/src/q43_동적계획법4/a_2169/Main.java
+++ BaekJoon/src/q43_동적계획법4/a_2169/Main.java
@@ -18,23 +18,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int dp[][]; 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), res = 0;
-        dp = new int[10][10];
-        int[] a = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-        for(int i = 0, tm = 0, mov; i < N; i++) {
-        	res += Math.abs(mov = dist(a[i] + tm, br.read()-'0'));
-        	tm += mov;
+        int N = Integer.parseInt(br.readLine()), dp[][] = new int[N+1][10];
+        int[] A = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        int[] B = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i <= N; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        for(int i = 0; i < 10; i++) dp[0][i] = i;
+        for(int i = 0; i < N; i++) for(int j = 0; j < 10; j++) {
+            int r = (B[i]-A[i]-j+20)%10, l = 10-r, k = (r+j)%10;
+            dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j]+l);
+            dp[i+1][k] = Math.min(dp[i+1][k], dp[i][j]+r);
         }
-        System.out.print(res);
-    }
-    static int dist(int a, int b) {
-    	if (a == b) return 0;
-    	if (a > b) {int tmp = a; a = b; b = tmp;}
-    	if (dp[a][b] != 0) return dp[a][b];
-    	int res = b - a;
-    	return dp[a][b] = res > 5 ? res - 10 : res;
+        System.out.print(Arrays.stream(dp[N]).min().getAsInt());
     }
 }
