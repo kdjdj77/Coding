8246a2cff9c50259302f44060df02be27cc1a8cb
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-09-06 21:43:37 +0900

    [Gold I] Title: 방법을 출력하지 않는 숫자 맞추기, Time: 136 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.java
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.java
@@ -1,19 +1,14 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), dp[][] = new int[N+1][10];
-        int[] A = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-        int[] B = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-        for(int i = 0; i <= N; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
-        for(int i = 0; i < 10; i++) dp[0][i] = i;
-        for(int i = 0; i < N; i++) for(int j = 0; j < 10; j++) {
-            int r = (B[i]-A[i]-j+20)%10, l = 10-r, k = (r+j)%10;
-            dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j]+l);
-            dp[i+1][k] = Math.min(dp[i+1][k], dp[i][j]+r);
+        int n = Integer.parseInt(br.readLine()), dp[][] = new int[n+1][10];
+        String a = br.readLine(), b = br.readLine();
+        while(n-- > 0) for(int i = 0; i < 10; i++) {
+            int s = (b.charAt(n)-'0'+10-((a.charAt(n)-'0'+i)%10))%10;
+            dp[n][i] = Math.min(dp[n+1][(s+i)%10]+s, dp[n+1][i]+10-s);
         }
-        System.out.print(Arrays.stream(dp[N]).min().getAsInt());
+        System.out.print(dp[0][0]);
     }
 }
