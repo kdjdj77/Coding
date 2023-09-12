ce736380446d326d9964efdc8b0a852085327ad5
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-09-12 22:11:09 +0900

    Update Main.java

--- BaekJoon/src/q43_동적계획법4/a_2494/Main.java
+++ BaekJoon/src/q43_동적계획법4/a_2494/Main.java
@@ -20,7 +20,13 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine()), dp[][] = new int[n+1][10];
+        String a = br.readLine(), b = br.readLine();
+        while(n-- > 0) for(int i = 0; i < 10; i++) {
+            int s = (b.charAt(n)-'0'+10-((a.charAt(n)-'0'+i)%10))%10;
+            dp[n][i] = Math.min(dp[n+1][(s+i)%10]+s, dp[n+1][i]+10-s);
+        }
+        System.out.print(dp[0][0]);
 		
 		
 	}	
