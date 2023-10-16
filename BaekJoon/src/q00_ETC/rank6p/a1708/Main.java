896259146020a7db351654ab933b3117cb22cba2
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-10-16 22:32:06 +0900

    Update Main.java

--- BaekJoon/src/q00_ETC/rank6p/a1708/Main.java
+++ BaekJoon/src/q00_ETC/rank6p/a1708/Main.java
@@ -24,50 +24,90 @@ package q00_ETC.rank6p.a1708;
 
 import java.io.*;
 import java.util.*;
-
-public class Main {
-	static Dot rt = new Dot(40001, 40001);
-    public static void main(String[] args) throws IOException {
+class Main{
+    static int n;
+    // x, y 절대값은 절대 40,000을 넘지 않음
+    static Point first = new Point(40001, 40001);
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Dot[] dots = new Dot[N];
-        for(int i = 0; i < N; i++) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        List<Point> points = new ArrayList<Point>();
+        for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
-            dots[i] = new Dot(x, y);
+            points.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-        for(int i = 0; i < N; i++) {
-            if (dots[i].y < rt.y) {rt = dots[i]; continue;}
-            if (dots[i].x < rt.x && dots[i].y == rt.y) rt = dots[i];
+        
+        // 점들 중, x좌표값이나, y좌표값이 가장 작은 점을 기준점으로 잡는다
+        for(int i=0; i<points.size(); i++){
+            if(points.get(i).y < first.y){
+	            first = points.get(i);    
+            } else if(points.get(i).y == first.y){
+                if(points.get(i).x < first.x)
+                    first = points.get(i);
+            }
         }
-        Arrays.sort(dots, new Comparator<Dot>() {
-            @Override
-            public int compare(Dot d1, Dot d2) {
-                int res = ccw(rt, d1, d2);
-                if (res == 0) {
-                	long dist1 = dist(rt, d1), dist2 = dist(rt, d2);
-                	return dist1 == dist2 ? 0 : dist1 > dist2 ? 1 : -1;
+        // 기준점과 나머지 점들이 ccw로 반시계방향(좌회전)이 되도록 정렬을 시키고, 만약 일직선상에 있으면 거리가 증가하게끔 정렬한다.
+        points.sort(new Comparator<Point>(){
+            /* 두 값을 처리한 리턴 결과가 음수,0,양수 이냐에 따라 첫번 째 인자의 정렬순위가 낮음, 같음,높음으로 정의된다.
+            compare(Product o1, Product o2)
+            ex) o1(1), 02(99)일 경우
+            return o2-o1일 때 return 결과가 양수임으로 o1의 우선순위가 더 높다(내림차순)
+            
+            ex) o1(1), o2(99)일 경우
+            reutrn o1-o2일 때는 return 결과가 음수이므로 o1의 우선순위가 더 낮다(오름차순)
+            
+            */
+            public int compare(Point second, Point third){
+                int ccwR = ccw(first, second, third);
+                if(ccwR > 0) // 반시계
+                    return -1; // 오름차순
+                else if (ccwR < 0)
+                    return 1; // 내림차순
+                else if (ccwR == 0){
+                    long distR1 = dist(first, second);
+                    long distR2 = dist(first, third);
+                    if(distR1 > distR2)
+                        return 1;
                 }
-                return res > 0 ? -1 : 1;
+                return -1;
             }
         });
-        Stack<Dot> s = new Stack<>() {{add(rt);}};
-        for(int i = 1; i < N; s.add(dots[i++])) while(s.size() > 1) {
-        	Dot cur = s.pop(), before = s.peek();
-        	if (ccw(before, cur, dots[i]) > 0) {s.add(cur); break;}
+        
+        // 그라함 스캔 알고리즘
+        Stack<Point> stack = new Stack<Point>();
+        stack.add(first);
+        for(int i=1; i<points.size(); i++){
+            // 시계방향이면 제거한다.
+            while(stack.size() > 1 && ccw(stack.get(stack.size()-2), stack.get(stack.size()-1), points.get(i)) <= 0){
+                stack.pop();
+            }
+            stack.add(points.get(i));
         }
-        System.out.print(s.size());
+        
+        bw.write(stack.size() + "\n");
+        
+        bw.flush();
+        br.close();
+        bw.close();
     }
-    static int ccw(Dot p1, Dot p2, Dot p3) {
-        long res = p1.x*p2.y + p2.x*p3.y + p3.x*p1.y - p2.x*p1.y - p3.x*p2.y - p1.x*p3.y;
-        return res == 0 ? 0 : res > 0 ? 1 : -1;
+    // ccw알고리즘 => 시계방향:-1, 일직선:0, 반시계:1
+    static int ccw(Point a, Point b, Point c){
+        long ccwR = (a.x*b.y + b.x*c.y + c.x*a.y) - (b.x*a.y + c.x*b.y + a.x*c.y);
+        if(ccwR > 0)
+            return 1;
+        if(ccwR < 0)
+            return -1;
+        return 0;
     }
-    static long dist(Dot p1, Dot p2) {
-    	long dx = p2.x-p1.x, dy = p2.y-p1.y;
-        return dx*dx + dy*dy;
+    static long dist(Point a, Point b){
+        return (b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y);
     }
-    static class Dot {
-        int x, y;
-        Dot(int x, int y) {this.x = x; this.y = y;}
+    static class Point{
+        long x, y;
+        public Point(long x, long y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
