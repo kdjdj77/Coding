4b05bbece3c2524bbe16e70c4d3371346db0ee8a
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-11-15 23:49:51 +0900

    Update Main.java

--- BaekJoon/src/q00_ETC/rank6p/a1006/Main.java
+++ BaekJoon/src/q00_ETC/rank6p/a1006/Main.java
@@ -30,8 +30,14 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-	
-	
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), W = Integer.parseInt(st.nextToken());
+		int[][] map = new int[2][N];
+		for(int i = 0; i < 2; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
 	}
 }
