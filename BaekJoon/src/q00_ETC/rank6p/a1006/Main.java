b81d201e28356d7efaec62c708d83c9092e637d3
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2023-12-04 22:25:46 +0900

    Update Main.java

--- BaekJoon/src/q00_ETC/rank6p/a1006/Main.java
+++ BaekJoon/src/q00_ETC/rank6p/a1006/Main.java
@@ -29,45 +29,31 @@ import java.util.*;
 public class Main {
 	static int N, W, a[], b[], c[], e[][];
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		for(int i = 0; i < T; i++) {
+		Reader fr = new Reader();
+		int T = fr.in();
+		while(T-- > 0) {
 			int res = Integer.MAX_VALUE;
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
+			N = fr.in(); W = fr.in();
 			e = new int[2][N];
-			for(int j = 0; j < 2; j++) {
-				st = new StringTokenizer(br.readLine());
-				for(int k = 0; k < N; k++) e[j][k] = Integer.parseInt(st.nextToken());
-			}
-			a = new int[N]; b = new int[N]; c = new int[N + 1];
+			
+			for(int j = 0; j < 2; j++) for(int k = 0; k < N; k++) e[j][k] = fr.in();
+			a = new int[N]; b = new int[N]; c = new int[N+1];
 			a[0] = b[0] = 1;
 			calc(0);
 			
 			res = Math.min(res, c[N]);
 			if (N > 1) {
 				if (e[0][0] + e[0][N-1] <= W && e[1][0] + e[1][N-1] <= W) {
-					a[1] = 1;
-					b[1] = 1;
-					c[1] = 0;
-					calc(1);
+					set(1, 1, 0);
 					res = Math.min(res, c[N-1] + 2);
 				}
 				if (e[0][0] + e[0][N-1] <= W) {
-					a[1] = 2;
-					b[1] = e[1][0] + e[1][1] > W ? 2 : 1;
-					c[1] = 1;
-					calc(1);
+					set(2, e[1][0] + e[1][1] > W ? 2 : 1, 1);
 					res = Math.min(res, b[N-1] + 1);
 				}
 				if (e[1][0] + e[1][N-1] <= W) {
-					a[1] = e[0][0] + e[0][1] > W ? 2 : 1;
-					b[1] = 2;
-					c[1] = 1;
-					calc(1);
+					set(e[0][0] + e[0][1] > W ? 2 : 1, 2, 1);
 					res = Math.min(res, a[N-1] + 1);
 				}
 			}
@@ -75,8 +61,11 @@ public class Main {
 		}
 		System.out.print(sb);
 	}
-	static void calc(int num) {
-		for(int i = num; i < N; i++) {
+	static void set(int n1, int n2, int n3) {
+		a[1] = n1; b[1] = n2; c[1] = n3; calc(1);
+	}
+	static void calc(int n) {
+		for(int i = n; i < N; i++) {
 			c[i+1] = Math.min(a[i] + 1, b[i] + 1);
 			if (e[0][i] + e[1][i] <= W) c[i+1] = Math.min(c[i+1], c[i] + 1);
 			if (i > 0 && e[0][i-1] + e[0][i] <= W && e[1][i-1] + e[1][i] <= W)
@@ -88,4 +77,10 @@ public class Main {
 			}
 		}
 	}
+}
+class Reader {
+	BufferedReader br; StringTokenizer st;
+    Reader(){br=new BufferedReader(new InputStreamReader(System.in));}
+    String ns(){if(st==null||!st.hasMoreTokens())try{st=new StringTokenizer(br.readLine());}catch(IOException e){}return st.nextToken();}
+    int in(){return Integer.parseInt(ns());}
 }
