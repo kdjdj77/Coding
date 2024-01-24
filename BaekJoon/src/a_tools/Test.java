b39b127cd5bf2b6032d346890e99f6b79d4b432a
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2024-01-25 01:24:19 +0900

    Update Test.java

--- BaekJoon/src/a_tools/Test.java
+++ BaekJoon/src/a_tools/Test.java
@@ -4,12 +4,6 @@ package a_tools;
 import java.io.*;
 import java.util.*;
 
-public class Test {
-	public static void main(String[] args) {
-		float[] test = new float[4];
-		for(int i = 0; i < 4; i++) {
-			System.out.println(test[i]);
-		}
-		
-	}
+public class Test{
+	
 }
